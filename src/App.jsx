dfdd33f6dac78d75efc27c6fb378247f5f9aad9b
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-20 14:49:31 +0200

    website scheme /w routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import ProductDetails from './pages/ProductDetails'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className='overflow-hidden'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/product/:id" element={<ProductDetails />}/>
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
   )
 }
 
