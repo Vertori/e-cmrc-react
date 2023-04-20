5fce4eed9d72a26b6bcdb2d94da9238327de13e6
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-20 15:13:58 +0200

    product context (fetching data)

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
-const ProductContext = () => {
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = () => {
+      axios
+        .get(`https://fakestoreapi.com/products`)
+        .then((response) => setProducts(response.data));
+    };
+    fetchProducts();
+  }, []);
+
   return (
-    <div>ProductContext</div>
-  )
-}
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
 
-export default ProductContext
+export default ProductProvider;
