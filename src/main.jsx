bbaa41b4fea2a477e69fe77187a92780a6f90d91
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-21 10:38:48 +0200

    sidebar context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import ProductProvider from './context/ProductContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ProductProvider from "./context/ProductContext.jsx";
+import SidebarProvider from "./context/SidebarContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ProductProvider>
-<React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </ProductProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <SidebarProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+  </SidebarProvider>
+);
