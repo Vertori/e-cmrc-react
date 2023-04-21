3e9cbb5ecb306c54103c4c765cfc5da85b12c092
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-21 14:08:07 +0200

    cart context, add to cart functionality

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,13 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { BsPlus, BsEyeFill} from 'react-icons/bs'
+import { CartContext } from '../context/CartContext'
+
 
 const Product = ({ product }) => {
   const { id, image, category, title, price } = product
+  const {addToCart} = useContext(CartContext)
+
   return (
     <div>
       <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition '>
@@ -15,7 +19,7 @@ const Product = ({ product }) => {
         </div>
         {/* buttons */}
         <div className='absolute top-6 -right-11 group-hover:right-5  p-2 flex flex-col justify-center items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
-          <button>
+          <button onClick={() => addToCart(product, id)}>
             <div className='flex justify-center items-center text-white w-12 h-12 bg-red-500'>
               <BsPlus className='text-3xl'/>
             </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,11 +4,24 @@ import { IoMdArrowForward } from 'react-icons/io'
 import { FiTrash2 } from 'react-icons/fi'
 import CartItem from '../components/CartItem'
 import { SidebarContext } from '../context/SidebarContext'
+import { CartContext } from '../context/CartContext'
+
 
 const Sidebar = () => {
   const {isOpen, handleClose} = useContext(SidebarContext)
+  const {cart} = useContext(CartContext)
   return (
-    <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] lg:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>Sidebar</div>
+    <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] lg:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
+      <div className='flex items-center justify-between py-6 border-b'>
+        <div className='uppercase text-sm font-semibold '>Shopping Cart (0)</div>
+        <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
+          <IoMdArrowForward className='text-2xl'/>
+        </div>
+      </div>
+      <div>{cart.map((item) =>{
+        return <CartItem item={item} key={item.id}/>
+      })}</div>
+    </div>
   )
 }
 
