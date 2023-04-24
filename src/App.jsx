82565c44a3b34f0a1a419906d4d3eb2e1f9e9c5f
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-24 19:44:15 +0200

    scroll restoration

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import ProductDetails from './pages/ProductDetails'
-import Sidebar from './components/Sidebar'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import WomensClothing from './pages/WomensClothing'
-import MensClothing from './pages/MensClothing'
-
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import WomensClothing from "./pages/WomensClothing";
+import MensClothing from "./pages/MensClothing";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  
-
   return (
-    <div className='overflow-hidden'>
+    <div className="overflow-hidden">
       <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/product/:id" element={<ProductDetails />}/>
-          <Route path="/women" element={<WomensClothing />}/>
-          <Route path="/men" element={<MensClothing />}/>
-        </Routes>
-        <Sidebar />
-        <Footer />
+        <ScrollToTop />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/women" element={<WomensClothing />} />
+            <Route path="/men" element={<MensClothing />} />
+          </Routes>
+          <Sidebar />
+          <Footer />
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
