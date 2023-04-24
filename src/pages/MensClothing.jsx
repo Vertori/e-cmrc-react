29a0752d8d3dd965b84b9a8744eeaddb855126c1
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-24 19:19:58 +0200

    women's clothing page, location nav redirecting

--- src/pages/MensClothing.jsx
+++ src/pages/MensClothing.jsx
@@ -6,49 +6,44 @@ import { BiChevronRight } from "react-icons/bi";
 import { AiOutlineHome } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
-
-
 const MensClothing = () => {
+  const { products } = useContext(ProductContext);
 
-    const { products } = useContext(ProductContext)
-
-const filteredMensProducts = products.filter((item) =>{
-    return item.category === "men's clothing"
-})
+  const filteredMensProducts = products.filter((item) => {
+    return item.category === "men's clothing";
+  });
 
   return (
     <div>
-        <GenderHero gender="Men's"/>
-    <section className="py-16 border-b ">
-      <div className="container mx-auto">
-        {/* location nav */}
-        <div className="pb-8 flex md:block"> 
-  <nav className='mx-auto px-4'>
-  <ol className="flex items-center gap-1 text-sm text-gray-600">
-<li>
-<Link to="/">
-  <AiOutlineHome/>
-</Link>
-</li>
-<li>
-<BiChevronRight />
-</li>
-<li>
-    Men's clothing
-</li>
-    </ol>
-  </nav>
+      <GenderHero gender="Men's" />
+      <section className="py-16 border-b ">
+        <div className="container mx-auto">
+          {/* location nav */}
+          <div className="pb-8 flex md:block">
+            <nav className="mx-auto px-4">
+              <ol className="flex items-center gap-1 text-sm text-gray-600">
+                <li>
+                  <Link to="/">
+                    <AiOutlineHome />
+                  </Link>
+                </li>
+                <li>
+                  <BiChevronRight />
+                </li>
+                <li>Men's clothing</li>
+              </ol>
+            </nav>
+          </div>
+          {/* displayed products */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm md:max-w-none mx-auto md:mx-0">
+            {filteredMensProducts.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
         </div>
-        {/* displayed products */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm md:max-w-none mx-auto md:mx-0">
-          {filteredMensProducts.map((product) => {
-            return <Product product={product} key={product.id} />;
-          })}
-        </div>
-      </div>
-    </section>
-  </div>
-  )
-}
+      </section>
+    </div>
+  );
+};
 
-export default MensClothing
+export default MensClothing;
